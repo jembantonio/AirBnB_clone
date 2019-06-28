df534452a7f7f40a3fbd6bea55630376d1c6349d
jembantonio <jembantonio@gmail.com>
2019-06-28 19:54:19 +0000

    pep8 fixes

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,11 @@ class BaseModel:
         if kwargs:
             for key, val in kwargs.items():
                 if key == 'created_at':
-                    self.created_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.strptime(val,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == 'updated_at':
-                    self.updated_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = datetime.strptime(val,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == 'id':
                     self.id = val
         else:
@@ -29,7 +31,9 @@ class BaseModel:
     def __str__(self):
         ''' Prints a BaseModel instance
         '''
-        return ("[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{:s}] ({:s}) {}".format(self.__class__.__name__,
+                                          self.id,
+                                          self.__dict__))
 
     def save(self):
         ''' Method that updates the public instance attribute updated_at with
@@ -43,7 +47,7 @@ class BaseModel:
         '''
         my_dict = {}
         for key, val in self.__dict__.items():
-            my_dict[key] = val 
+            my_dict[key] = val
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['__class__'] = self.__class__.__name__
