e06a5b2303c845fc4b619bbcd2f43abb2edbb6b7
root <root@JohnsPC.localdomain>
2019-06-28 12:56:48 -0700

    got kwargs working

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,21 @@ from datetime import datetime
 class BaseModel:
     ''' BaseModel Class
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' Initializes the BaseModel class
         '''
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'id':
+                    self.id = val
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         ''' Prints a BaseModel instance
@@ -32,10 +41,10 @@ class BaseModel:
         ''' Method that returns a dictionary containing all key/value pairs of
             an instance
         '''
-        dict = {}
+        my_dict = {}
         for key, val in self.__dict__.items():
-            dict[key] = val 
-        dict['created_at'] = self.created_at.isoformat()
-        dict['updated_at'] = self.updated_at.isoformat()
-        dict['__class__'] = self.__class__.__name__
-        return dict
+            my_dict[key] = val 
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
