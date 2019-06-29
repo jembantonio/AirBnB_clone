a08bc5a526dbef51d778611b633af53723336ccf
jembantonio <jembantonio@gmail.com>
2019-06-29 00:59:06 +0000

    revised save method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,38 +13,29 @@ class FileStorage:
         __file_path = 'file.json'
         __objects = {}
 
-    @property
     def file_path(self):
         '''getter for file_path
         '''
         return self.__file_path
 
-    @property
     def all(self):
         '''getter for the dict _objects
         '''
         return self.__objects
 
-    @new.setter
     def new(self, obj):
         '''setter for __objects
         '''
-        self.__objects = obj
-    
-    @classmethod
-    def save(self):
-        with open (self.file_path, mode='w', encoding='utf-8') as my_file: 
-            f = {}
-            for my_dict in file_data:
-                dict_obj = BaseModel.to_dict(**my_dict)
-               f.append(dict_obj)
-            my_file.write(json.dumps(self.all))
+        key = self.__class__.__name__ + "." + self.id
+        self.__objects[key] = obj
 
-    @classmethod
+    def save(self):
+        dict_dict = {}
+        for key in self.all():
+            turn_dict = self.all()
+            dict_dict[self.new()] = turn_dict.to_dict()
+        with open (self.file_path, mode='w', encoding='utf-8') as my_file: 
+            json.dumps(dict_dict, my_file)
+
     def reload(self):
-        import os
-        from #import base_model.py
-        if os.path.exists(self.file_path):
-            with open (self.file_path, mode='r', encoding='utf-8') as my_file:
-              # file_data = json.loads(my_file.read())
                
