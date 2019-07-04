a7de73f61d8ec8926441ec6ffc56b0047107ef7f
root <root@JohnsPC.localdomain>
2019-07-03 17:45:47 -0700

    a few unit test for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,7 +3,20 @@
 '''
 
 import unittest
-from models.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 
 class Test_Filestorage(unittest.TestCase):
+
+    def test_file_path(self):
+        fs = FileStorage()
+        assert isinstance(fs._FileStorage__file_path, str)
+
+    def test_objects(self):
+        fs = FileStorage()
+        assert isinstance(fs._FileStorage__objects, dict)
+
+    def test_all(self):
+        fs = FileStorage()
+        self.assertEqual(fs.all(), fs._FileStorage__objects)
+
